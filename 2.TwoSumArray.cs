0df1254a4309797d168d8ccc1e3c53323c711e5d
Michael Mburu <ichangaimichael@yahoo.com>
2022-12-23 22:43:08 +0300

    Added twoSumArray v3 using hashtable O(n) add, O(1) find/lookup

---
+++ 2.TwoSumArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Application
+{
+    //OLog(n)
+    public int[] twoSumV2(int[] numbers, int target)
+    {
+        //Assume is already sorted
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int j = bsearch(numbers, target - numbers[i], i + 1);
+        }
+
+        throw new ArgumentException("No two sum solution");
+    }
+
+    private int bsearch(int[] A, int key, int start)
+    {
+        int L = start, R = A.Length - 1;
+        while (L < R)
+        {
+            int M = (L + R) / 2;
+            if (A[M] < key)
+            {
+                L = M + 1;
+            }
+            else
+            {
+                R = M;
+            }
+        }
+
+        return (L == R && A[L] == key) ? L : -1;
+    }
+
+    //O(n)
+    public int[] twoSumV3(int[] numbers, int target)
+    {
+        //Assume input is already sorted
+        int i = 0, j = numbers.Length - 1;
+        while (i < j)
+        {
+            int sum = numbers[i] + numbers[j];
+            if (sum < target)
+            {
+                //If Sum is less than target incrementing j will make sum smaller so we increment i
+                i++;
+            }
+            else if (sum > target)
+            {
+                //If sum is greater than target, decrementing i will make the sum larger so we decrement j
+                j--;
+            }
+            else
+            {
+                return new int[] { i + 1, j + 1 };
+            }
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
+
---
+++ LeetCode50Exercises/3.TwoSumArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace LeetCode50Exercises
+{
+    public class twoSign
+    {
+        /*
+            Design and impliment a TwoSum class. It should support following operations add and find
+            add(input)
+            find(value)
+
+            Example: add(1) add(2) add(5): find(4) > True, find(7) false
+
+            Solution: Store all possible sums in a hash table, add goes through the list and stores new pair sums
+            Find operation involves a single hash table lookup O(1)
+        */
+
+        //Add O(n), Find O(1)
+        private Dictionary<int, int> table = new Dictionary<int, int>();
+
+        public void add(int input)
+        {
+            //Check if table contains input and add if not
+            int count = table.ContainsKey(input) ? table.GetValueOrDefault(input) : 0;
+            table.Add(input, count + 1);
+        }
+
+        public Boolean find(int val)
+        {
+            Dictionary<int, int> entry = table;
+            int num = entry.GetValueOrDefault(val);
+            int y = val - num;
+            if(y == num)
+            {
+                //For duplicates ensure there is two individual numbers
+                if(entry.GetValueOrDefault(y) >= 2)
+                {
+                    return true;
+                }
+                else if (table.ContainsKey(y))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
+
